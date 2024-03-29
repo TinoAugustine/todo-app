b4bda7163c46192b3f49d641dc9d9d7e91d7b21e
TinoAugustine <12378397+TinoAugustine@users.noreply.github.com>
2024-03-29 21:55:36 +1100

    Implemented ADD Button

--- Bonus/APP-File-Zip.py
+++ Bonus/APP-File-Zip.py
@@ -1,17 +1,37 @@
 import PySimpleGUI as sg
+import zipfile
+import os
 
-label1 = sg.Text("Select Files to compress:")
-input1 = sg.Input()
-choose_button1 = sg.FilesBrowse("Choose")
+def compress_files(file_list, destination_folder):
+    zip_file_path = os.path.join(destination_folder, 'compressed_files.zip')
+    with zipfile.ZipFile(zip_file_path, 'w') as zipf:
+        for file_path in file_list:
+            zipf.write(file_path, os.path.basename(file_path))
+    return zip_file_path
 
-label2 = sg.Text("Select Files to compress:")
-input2 = sg.Input()
-choose_button2 = sg.FolderBrowse("Choose")
+layout = [
+    [sg.Text("Select Files to compress:")],
+    [sg.Input(key="-FILES-", enable_events=True, visible=False), sg.FilesBrowse("Choose", key="-BROWSE-")],
+    [sg.Listbox(values=[], size=(60, 6), key="-FILELIST-")],
+    [sg.Text("Select Destination Folder:")],
+    [sg.Input(key="-FOLDER-"), sg.FolderBrowse("Choose")],
+    [sg.Button("Compress"), sg.Button("Exit")]
+]
 
-compress_button = sg.button("Compress")
+window = sg.Window("File Compressor", layout)
 
-window = sg.Window("File Compressor",
-                   layout=[[label1, input1, choose_button1],
-                           [label2, input2, choose_button2]])
-window.read()
+while True:
+    event, values = window.read()
+    if event == sg.WINDOW_CLOSED or event == "Exit":
+        break
+    elif event == "-BROWSE-":
+        selected_files = values["-BROWSE-"].split(';')
+        window["-FILELIST-"].update(selected_files)
+    elif event == "Compress":
+        file_list = values["-FILELIST-"]
+        destination_folder = values["-FOLDER-"]
+        if file_list and destination_folder:
+            zip_file_path = compress_files(file_list, destination_folder)
+            sg.popup(f"Files compressed successfully!\nZip file saved at:\n{zip_file_path}")
+
 window.close()
--- gui.py
+++ gui.py
@@ -1,11 +1,23 @@
 import functions
 import PySimpleGUI as sg
 
-lable = sg.Text("Type in Too Do")
-input_box = sg.InputText(tooltip="Enter Your Todo")
+lable = sg.Text("Type in To-Do")
+input_box = sg.InputText(tooltip="Enter Your Todo", key="todo")
 add_button = sg.Button("Add")
 
-window = sg.Window('My To Do App', [[lable], [input_box], [add_button]])
-window.read()
-print("Hello World")
+window = sg.Window('My To Do App',
+                   [[lable], [input_box], [add_button]],
+                   font=('Helvetica',20))
+while True:
+    event, values = window.read()
+    print(event)
+    print(values)
+    match event:
+        case "Add":
+            todos = functions.get_todos()
+            new_todos = values['todo'] + "\n"
+            todos.append(new_todos)
+            functions.write_todos(todos)
+        case sg.WIN_CLOSED:
+            break
 window.close()
